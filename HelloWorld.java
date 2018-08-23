7267273fc7057cd400220b5143bb42317a761a62
mallesh <malpraskarthikrao@gmail.com>
2018-08-23 10:54:29 -0700

    again initial  Commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,10 +1,6 @@
-//This is q Hellow world progfram
 public class HelloWorld {
    public static void main(String[] args) {
       // Prints "Hello, World" in the terminal window.
-      // Added new comment for 4th dition
-      System.out.println("Helooooo   GIT");
-      System.out.println("Hello AWS"
       System.out.println("Hello, World");
    }
 }
