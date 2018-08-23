ea62245715d54c727088a495696b6a950609f282
mallesh <malpraskarthikrao@gmail.com>
2018-08-23 05:59:58 -0700

    5 th Commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,9 @@
 public class HelloWorld {
    public static void main(String[] args) {
       // Prints "Hello, World" in the terminal window.
+      // Added new comment for 4th dition
+      System.out.println("Helooooo   GIT");
+      System.out.println("Hello AWS"
       System.out.println("Hello, World");
    }
 }
